297aaf8ffaa6f8dabcf786e0e2b1d148aa6d1db9
Elle-Dorvil <rachelle.dorvil@yahoo.com>
2025-04-10 09:28:52 -0400

    Initial Commit

---
+++ workbook-1/ShoppingListProject/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class ShoppingList {
+   public static void main(String[] args) {
+        System.out.println("Eggs");
+    System.out.println("sugar");
+    System.out.println("candy");
+    System.out.println("hamburger");
+    System.out.println("bread");
+    System.out.println("chicken");
+    System.out.println("cake");
+    System.out.println("juice");
+    System.out.println("ground beef");
+    System.out.println("shrimp");
+}   }
---
+++ workbook-1/first-mavern-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hey Elle is the name and I hope I catch on to this");
+        System.out.println("Im excited");
+    }
+}
