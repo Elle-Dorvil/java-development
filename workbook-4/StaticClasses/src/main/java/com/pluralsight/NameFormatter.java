f31cc9aa2f6f2029ebd36466dfc92dc9488996b7
Elle-Dorvil <rachelle.dorvil@yahoo.com>
2025-05-08 10:22:46 -0400

    trying to create the methods inside of the static class

---
+++ workbook-4/StaticClasses/src/main/java/com/pluralsight/NameFormatter.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+public class NameFormatter {
+
+
+
+private NameFormatter() {}
+
+    public static String formatter(String prefix, String firstName, String middleName, String lastName, String suffix){
+    StringBuilder newBuilder = new StringBuilder();
+    return prefix + firstName + middleName + lastName + suffix;
+    }
+    public static String formatter(String firstName, String lastName) {
+    return firstName + lastName;
+    }
+    public static String formatter(fullName) {
+
+    }
+
+
+
+
+
+}
