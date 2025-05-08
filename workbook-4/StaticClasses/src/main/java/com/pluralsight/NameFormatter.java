745b04fa518b7566216529835e66f7ae9ece3052
Elle-Dorvil <rachelle.dorvil@yahoo.com>
2025-05-08 10:34:01 -0400

    Correct the order

--- workbook-4/StaticClasses/src/main/java/com/pluralsight/NameFormatter.java
+++ workbook-4/StaticClasses/src/main/java/com/pluralsight/NameFormatter.java
@@ -6,13 +6,15 @@ public class NameFormatter {
 
 private NameFormatter() {}
 
+    public static String formatter(String firstName, String lastName) {
+        return firstName + lastName;
+    }
+
     public static String formatter(String prefix, String firstName, String middleName, String lastName, String suffix){
     StringBuilder newBuilder = new StringBuilder();
     return prefix + firstName + middleName + lastName + suffix;
     }
-    public static String formatter(String firstName, String lastName) {
-    return firstName + lastName;
-    }
+
     public static String formatter(fullName) {
 
     }
